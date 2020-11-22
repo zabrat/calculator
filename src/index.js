da1939a1c4b8a5f43ce2e61b5b012535fea7bc68
zabrat <zaur1996@bk.ru>
2020-11-22 15:50:11 +0300

    fixed dot problem

--- src/index.js
+++ src/index.js
@@ -1,49 +1,58 @@
-const mainLineEl = document.querySelector('#mainLine')
-const numbersButtonEl = document.querySelectorAll('button')
-const staticDisplayEl = document.querySelector('.static-display')
-let interactiveDisplay = ''
+const mainLineEl = document.querySelector('#mainLine');
+const numbersButtonEl = document.querySelectorAll('button');
+const staticDisplayEl = document.querySelector('.static-display');
+let interactiveDisplay = '';
 
 function fixInStaticDisplay () {
-  const result = customEval(interactiveDisplay)
-  const history = document.createElement('span')
-  history.innerHTML = `${interactiveDisplay} = ${result}`
-  staticDisplayEl.appendChild(history)
-  interactiveDisplay = ''
-  showSymbol(result)
+  const result = customEval(interactiveDisplay);
+  const history = document.createElement('span');
+
+  history.innerHTML = `${interactiveDisplay} = ${result}`;
+  staticDisplayEl.appendChild(history);
+  interactiveDisplay = '';
+  showSymbol(result);
 }
 
-function addMinus () {
-  if (interactiveDisplay.length === 0) {
-    showSymbol('-')
+function checkDot(str) {
+    const arr = str.trim().split(' ');
+    const operators = ['+', '-', '÷', '×', '(', ')', '.'];
+    if (operators.includes(arr[arr.length - 1]) || arr[arr.length - 1].includes('.')){
+        ;
+    } else {
+        showSymbol('.');
+    }
+}
+function addMinus (str) {
+  if (str.length === 0) {
+    showSymbol('-');
   } else if (
-    ['+', '-', '/', '×', '(', ')'].includes(
-      interactiveDisplay.trim()[interactiveDisplay.trim().length - 1]
-    )
+    ['+', '-', '÷', '×', '(', ')'].includes(
+      str.trim()[str.trim().length - 1])
   ) {
-    showSymbol('-')
+    showSymbol('-');
   } else {
-    showSymbol(' - ')
+    showSymbol(' - ');
   }
 }
 
 function root () {
-  const splitter = interactiveDisplay.split(' ')
-  const rooted = Math.sqrt(splitter[splitter.length - 1])
+  const splitter = interactiveDisplay.split(' ');
+  const rooted = Math.sqrt(splitter[splitter.length - 1]);
 
-  splitter.splice(-1, 1)
-  splitter.push(rooted)
-  interactiveDisplay = splitter.join(' ')
-  mainLineEl.innerHTML = interactiveDisplay
+  splitter.splice(-1, 1);
+  splitter.push(rooted);
+  interactiveDisplay = splitter.join(' ');
+  mainLineEl.innerHTML = interactiveDisplay;
 }
 
 function power () {
-  const splitter = interactiveDisplay.split(' ')
-  const powered = Math.pow(splitter[splitter.length - 1], 2)
+  const splitter = interactiveDisplay.split(' ');
+  const powered = Math.pow(splitter[splitter.length - 1], 2);
 
-  splitter.splice(-1, 1)
-  splitter.push(powered)
-  interactiveDisplay = splitter.join(' ')
-  mainLineEl.innerHTML = interactiveDisplay
+  splitter.splice(-1, 1);
+  splitter.push(powered);
+  interactiveDisplay = splitter.join(' ');
+  mainLineEl.innerHTML = interactiveDisplay;
 }
 
 function backspace () {
@@ -59,161 +68,162 @@ function backspace () {
     )
   }
 
-  mainLineEl.innerHTML = interactiveDisplay
+  mainLineEl.innerHTML = interactiveDisplay;
 }
 
 function showSymbol (arg) {
   if (arg === 'C') {
-    interactiveDisplay = ''
-    staticDisplayEl.innerHTML = ''
+    interactiveDisplay = '';
+    staticDisplayEl.innerHTML = '';
   } else {
-    interactiveDisplay += arg
+    interactiveDisplay += arg;
   }
 
-  mainLineEl.innerHTML = interactiveDisplay
+  mainLineEl.innerHTML = interactiveDisplay;
 }
 
-function countInsideBraces (str) {
-  const start = str.indexOf('(')
-  const end = str.lastIndexOf(')')
-  const betweenBraces = str.slice(start + 1, end)
-  const res = customEval(betweenBraces.join(' '))
-  const len = betweenBraces.length + 2
+function countInsideBraces (arr) {
+  const start = arr.indexOf('(');
+  const end = arr.lastIndexOf(')');
+  const betweenBraces = arr.slice(start + 1, end);
+  const res = customEval(betweenBraces.join(' '));
+  const len = betweenBraces.length + 2;
 
-  str.splice(start, len, res)
+  arr.splice(start, len, res);
 
-  return str
+  return arr;
 }
 
 function customEval (str) {
-  str = str.split(' ')
+  const arr = str.split(' ');
 
-  if (str.includes('(') || str.includes(')')) {
-    countInsideBraces(str)
-    if (str.length === 1) {
-      return str[0]
+  if (arr.includes('(') || arr.includes(')')) {
+    countInsideBraces(arr);
+    if (arr.length === 1) {
+      return arr[0];
     }
   }
 
-  if (str.includes('×') || str.includes('/')) {
-    MultiDivide(str)
-    if (str.length === 1) {
-      return str[0]
+  if (arr.includes('×') || arr.includes('÷')) {
+    MultiDivide(arr);
+    if (arr.length === 1) {
+      return arr[0];
     }
   }
 
-  plusMinus(str)
-  if (str.length === 1) {
-    return str[0]
+  plusMinus(arr)
+  if (arr.length === 1) {
+    return arr[0];
   }
 }
 
-function MultiDivide (str) {
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === '/') {
-      const res = Number(str[i - 1]) / Number(str[i + 1])
-      str.splice(i - 1, 3, res)
-      i -= 1
+function MultiDivide (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '÷') {
+      const res = Number(arr[i - 1]) / Number(arr[i + 1]);
+
+      arr.splice(i - 1, 3, res);
+      i -= 1;
     }
 
-    if (str[i] === '×') {
-      const res = Number(str[i - 1]) * Number(str[i + 1])
-      str.splice(i - 1, 3, res)
-      i -= 1
+    if (arr[i] === '×') {
+      const res = Number(arr[i - 1]) * Number(arr[i + 1]);
+      arr.splice(i - 1, 3, res);
+      i -= 1;
     }
   }
 
-  return str
+  return arr;
 }
 
-function plusMinus (str) {
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === '+') {
-      const res = Number(str[i - 1]) + Number(str[i + 1])
-      str.splice(i - 1, 3, res)
-      i -= 1
+function plusMinus (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '+') {
+      const res = Number(arr[i - 1]) + Number(arr[i + 1]);
+      arr.splice(i - 1, 3, res);
+      i -= 1;
     }
-    if (str[i] === '-') {
-      const res = Number(str[i - 1]) - Number(str[i + 1])
-      str.splice(i - 1, 3, res)
-      i -= 1
+    if (arr[i] === '-') {
+      const res = Number(arr[i - 1]) - Number(arr[i + 1]);
+      arr.splice(i - 1, 3, res);
+      i -= 1;
     }
   }
-  return str
+  return arr;
 }
 
 for (const i of numbersButtonEl) {
   i.addEventListener('click', () => {
     switch (i.id) {
       case 'one':
-        showSymbol('1')
-        break
+        showSymbol('1');
+        break;
       case 'two':
-        showSymbol('2')
-        break
+        showSymbol('2');
+        break;
       case 'three':
-        showSymbol('3')
-        break
+        showSymbol('3');
+        break;
       case 'four':
-        showSymbol('4')
-        break
+        showSymbol('4');
+        break;
       case 'five':
-        showSymbol('5')
-        break
+        showSymbol('5');
+        break;
       case 'six':
-        showSymbol('6')
-        break
+        showSymbol('6');
+        break;
       case 'seven':
-        showSymbol('7')
-        break
+        showSymbol('7');
+        break;
       case 'eight':
-        showSymbol('8')
-        break
+        showSymbol('8');
+        break;
       case 'nine':
-        showSymbol('9')
-        break
+        showSymbol('9');
+        break;
       case 'zero':
-        showSymbol('0')
-        break
+        showSymbol('0');
+        break;
       case 'doubleZero':
-        showSymbol('00')
-        break
+        showSymbol('00');
+        break;
       case 'dot':
-        showSymbol('.')
-        break
+        checkDot(interactiveDisplay);
+        break;
       case 'division':
-        showSymbol(' / ')
-        break
+        showSymbol(' ÷ ');
+        break;
       case 'back':
-        backspace()
-        break
+        backspace();
+        break;
       case 'clear':
-        showSymbol('C')
-        break
+        showSymbol('C');
+        break;
       case 'multi':
-        showSymbol(' × ')
-        break
+        showSymbol(' × ');
+        break;
       case 'frontBrace':
-        showSymbol('( ')
-        break
+        showSymbol('( ');
+        break;
       case 'backBrace':
-        showSymbol(' )')
-        break
+        showSymbol(' )');
+        break;
       case 'minus':
-        addMinus()
-        break
+        addMinus(interactiveDisplay);
+        break;
       case 'power':
-        power()
-        break
+        power();
+        break;
       case 'root':
-        root()
-        break
+        root();
+        break;
       case 'plus':
-        showSymbol(' + ')
-        break
+        showSymbol(' + ');
+        break;
       case 'equally':
-        fixInStaticDisplay()
-        break
+        fixInStaticDisplay();
+        break;
     }
-  })
+  });
 }
